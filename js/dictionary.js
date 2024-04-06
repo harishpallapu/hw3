d3774e473be5973e71aca61be6f4328e3fbef11c
harishpallapu <81285240+harishpallapu@users.noreply.github.com>
2024-04-05 22:23:32 -0500

    Add files via upload

---
+++ js/dictionary.js
@@ -0,0 +1,32 @@
+function search() {
+    const term = document.getElementById("search-input").value.trim().toLowerCase();
+    const results = document.getElementById("result-list");
+    results.innerHTML = "";
+
+    fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${term}`)
+        .then(response => response.json())
+        .then(data => {
+            if (data.title === "No Definitions ") {
+                results.innerHTML = "<li>No results found</li>";
+            } else {
+                let count = 1;
+                data.forEach(entry => {
+                    const word = entry.word.toLowerCase(); 
+                    if (word === term) {
+                        const partOfSpeech = entry.meanings[0].partOfSpeech;
+                        const definition = entry.meanings[0].definitions[0].definition;
+                        results.innerHTML += `<li>${count}. ${word} (${partOfSpeech}): ${definition}</li>`;
+                        count++;
+                    }
+                });
+                if (count === 1) {
+                    results.innerHTML = "<li>No exact match found</li>";
+                }
+            }
+        });
+
+
+    results.style.marginTop = "10px";
+}
+
+document.getElementById("search-input").addEventListener("input", search);
